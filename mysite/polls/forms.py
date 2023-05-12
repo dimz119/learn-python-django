0fd307a1355afeff6e29a882efb8c69fb51a29a9
Seungjoon Lee <joonlee0228@gmail.com>
2023-05-11 22:01:09 -0700

    added style

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 
 class SurveyForm(forms.Form):
-    user_name = forms.CharField(label='Your name', max_length=100)
-    user_age = forms.IntegerField(label='Your age')
+    # https://docs.djangoproject.com/en/4.1/ref/forms/widgets/#built-in-widgets
+    user_name = forms.CharField(
+        label='Your name',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    user_age = forms.IntegerField(
+        label='Your age',
+        widget=forms.NumberInput(attrs={'class': 'form-control'}))
