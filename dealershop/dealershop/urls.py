ccf2ef35dc8e1c15ac0cf5df33ab6f78b71e8569
Seungjoon Lee <joonlee0228@gmail.com>
2023-05-16 22:52:05 -0700

    redirect view

--- dealershop/dealershop/urls.py
+++ dealershop/dealershop/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inventory/', include('inventory.urls')),
+    path('', RedirectView.as_view(url='inventory/')),
 ]
