727ce1e4291139e5d03103c4d521c909e07829d3
Seungjoon Lee <joonlee0228@gmail.com>
2023-05-20 09:00:35 -0700

    added req and resp

--- app/snippets/urls.py
+++ app/snippets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
