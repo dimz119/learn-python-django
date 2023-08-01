f6de98d64738674d6701981ea7cf87b809660d16
Seungjoon Lee <joonlee0228@gmail.com>
2023-07-31 22:45:34 -0700

    added django_channels_tutorial1

--- django_channels/django_channels/asgi.py
+++ django_channels/django_channels/asgi.py
@@ -3,12 +3,11 @@ import os
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_channels.settings')
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_channels.settings")
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
-})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        # Just HTTP for now. (We can add other protocols later.)
+    }
+)
