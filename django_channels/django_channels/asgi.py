23a635cc056ee240b482bd3f6619647abffc0bac
Seungjoon Lee <joonlee0228@gmail.com>
2023-07-31 23:17:16 -0700

    added django_channels_tutorial2

--- django_channels/django_channels/asgi.py
+++ django_channels/django_channels/asgi.py
@@ -1,13 +1,24 @@
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+from chat.routing import websocket_urlpatterns
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_channels.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+import chat.routing
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
-        # Just HTTP for now. (We can add other protocols later.)
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
     }
 )
--- django_channels/django_channels/settings.py
+++ django_channels/django_channels/settings.py
@@ -126,3 +126,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ASGI_APPLICATION = "django_channels.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
