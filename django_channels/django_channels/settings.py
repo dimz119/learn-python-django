564bbd37e0ac579dd207fca6c10770e17e347162
Seungjoon Lee <joonlee0228@gmail.com>
2023-08-01 22:49:02 -0700

    add django_channels_tutorial4

--- django_channels/django_channels/settings.py
+++ django_channels/django_channels/settings.py
@@ -80,6 +80,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        "TEST": {
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
     }
 }
 
